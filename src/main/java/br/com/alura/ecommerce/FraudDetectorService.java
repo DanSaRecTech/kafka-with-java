d3b9e7cb21765f44e0f9b4be2eff4f22e2380633
Dan Araújo <dansa.rectech@gmail.com>
2022-05-10 00:45:57 -0300

    Criando camadas para separar os serviços de consumer e producer

--- src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,54 +1,36 @@
 package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.time.Duration;
-import java.util.Collections;
 import java.util.Properties;
 
 public class FraudDetectorService {
     public static void main(String[] args) {
-        //crio um kafka consume, onde irei esperar a chave e valor como String e passo suas properties.
-        var consumer = new KafkaConsumer<String, String>(properties());
-        //informo qual o tópico que irei consumir. Geralmente, consumimos apenas 1 tópico.
-        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
-        //Fico rodando e escutando algum dado ser lançado no produce.
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(100));
-            if(!records.isEmpty()) {
-                System.out.println("encontrei " + records.count() +  "registros");
-                for(var record : records) {
-                    System.out.println("-------------------------------------------");
-                    System.out.println("Processing new order, checking for fraud");
-                    System.out.println(record.key());
-                    System.out.println(record.value());
-                    System.out.println(record.partition());
-                    System.out.println(record.offset());
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        //ignoring
-                        e.printStackTrace();
-                    }
-                    System.out.println("Order processed");
-                }
-            }
+
+        var fraudeService = new FraudDetectorService();
+        var service = new KafkaService(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER", fraudeService::parse);
+        service.run();
+    }
+
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("-------------------------------------------");
+        System.out.println("Processing new order, checking for fraud");
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("Order processed");
     }
 
     private static Properties properties() {
         var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        //Deserializa de Byte para String a chave
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        //Deserializa de Byte para String o value
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        //pega o grupo id
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
-        //No máximo, de 1 em 1, ele vai comitando as mensagens.
-        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
         return properties;
     }
 }
